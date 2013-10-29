213202152a2a79c35d9338e7cc135151c5050751
Apprentice <apprentice@dbc-chicago-004.local>
2013-10-29 14:17:19 -0500

    next

--- test_spec.rb
+++ test_spec.rb
@@ -2,10 +2,18 @@ require  "rspec"
 
 require_relative "test"
 
+let(:list) { List.new("Hankaconda")}
+
 describe List do
   describe "#initialize" do
     it "should create an instance of List" do
-      List.new.should be_an_instance_of List
+      list.should be_an_instance_of List
     end
+
+    it "requires a title argument" do
+      expect {List.new}.to raise_error(ArgumentError)
+    end
+
+
   end
 end
