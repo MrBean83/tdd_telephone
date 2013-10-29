28689f907bd0bb9147c0790a647c70a052d7833d
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 15:36:56 -0500

    yeahhh

--- test.rb
+++ test.rb
@@ -1,10 +1,20 @@
-class List
- attr_reader :title
+class Task
+ attr_reader :title, :description
 
   def initialize(title)
   @title = title
+  @description = description
+  @task_list = 0
 
+  end
 
+  def status
+    case
+    when @task_list < 3
+      "incomplete"
+    else
+      raise "you win"
+    end
   end
 
 end
